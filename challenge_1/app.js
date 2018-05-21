b9f88a94a838743e5de8d4f93610e60597d2f41e
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-21 13:20:20 -0700

    tarcking user

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,9 +1,34 @@
+var userTurn = 'A';
+
 function clickEvent(){
-this.innerHTML=  'X';
+if(userTurn === 'A') {
+  this.innerHTML=  'X';
+} else {
+  this.innerHTML=  'O';
+}
+
+}
+function clickEventB(){
+  this.innerHTML = 'O';
+  }
+function switchUser(){
+  userTurn === 'A' ? userTurn = 'B' : userTurn = 'A';
+  console.log(userTurn)
 }
 var arr = document.getElementsByClassName("squre");
-for(var i = 0; i < arr.length;i++) {
-  arr[i].addEventListener("click", clickEvent);
+
+function isItEmpty(){
+  for(var i = 0; i < arr.length;i++) {
+    if(arr[i].innerHTML === 'X' || arr[i].innerHTML === 'O'){
+      alert('err')
+    } 
+  }
+  alert('start')
 }
+for(var i = 0; i < arr.length;i++) {
+    arr[i].addEventListener("click", clickEvent);
+    arr[i].addEventListener("click", switchUser);
+}
+isItEmpty()
 console.log('hi')
 //.addEventListener("click", clickEvent);
