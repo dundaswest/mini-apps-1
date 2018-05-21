d93b8a9464262263a10ad5683a1dfc1640e57a71
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-21 16:16:49 -0700

    check horizontly

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,21 +1,45 @@
 var userTurn = 'A';
+var currentBoard = [];
+var arr = document.getElementsByClassName("squre");
+//make empty board
+console.log(arr)
+function myBoard() {
+  var board = [];
+  for(var i = 0; i < arr.length;i++) {
+      var count = 0;
+      board.push(arr[i].innerHTML.trim());
+  }
+  currentBoard.push(board.slice(0,3));
+  currentBoard.push(board.slice(3,6));
+  currentBoard.push(board.slice(6,9));
+}
+  
+myBoard();
+console.log(currentBoard)
 
 function clickEvent(){
-if(userTurn === 'A') {
-  this.innerHTML=  'X';
-} else {
-  this.innerHTML=  'O';
-}
-
-}
-function clickEventB(){
+  if(userTurn === 'A') {
+    console.log(this)
+    console.log('innerText!!' + this.textContent)
+   this.innerHTML = 'X';
+  } else {
   this.innerHTML = 'O';
   }
+ // myBoard();
+  horizontally();
+  vertically()
+}
+
+function clickEventB(){
+  this.innerHTML = 'O';
+}
+
 function switchUser(){
   userTurn === 'A' ? userTurn = 'B' : userTurn = 'A';
-  console.log(userTurn)
+  //console.log(userTurn);
 }
-var arr = document.getElementsByClassName("squre");
+
+//todo implement board
 
 function isItEmpty(){
   for(var i = 0; i < arr.length;i++) {
@@ -29,6 +53,24 @@ for(var i = 0; i < arr.length;i++) {
     arr[i].addEventListener("click", clickEvent);
     arr[i].addEventListener("click", switchUser);
 }
-isItEmpty()
-console.log('hi')
+isItEmpty();
+function horizontally(){
+  if(currentBoard.some(e => e.join('') === 'XXX' || e.join('') === 'OOO')) {
+    alert('hiiiiiii')
+  }
+}
+function vertically() {
+  for(var  i = 0; i < currentBoard.length;i++) {
+    for(var j = 0; j < currentBoard.length;j++) {
+      let arr = [];
+      arr.push([currentBoard[i][j]]);
+    }
+   
+  }
+  let first = currentBoard.filter((e) => e[i][0] === 0);
+
+  console.log('vertical ' + first)
+}
+console.log('hi');
+console.log(currentBoard)
 //.addEventListener("click", clickEvent);
