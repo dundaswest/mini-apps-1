9d2fc846ca4f433d80cb8d4b9b26b8097b18e97a
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-21 12:19:35 -0700

    add eventlistenr on each squre

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,9 @@
-
-
+function clickEvent(){
+console.log('click');
+}
+var arr = document.getElementsByClassName("squre");
+for(var i = 0; i < arr.length;i++) {
+  arr[i].addEventListener("click", clickEvent);
+}
+console.log('hi')
+//.addEventListener("click", clickEvent);
