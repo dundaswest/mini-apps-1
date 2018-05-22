30409275a39b0d7cd0c8bf5f8d79fa242d007195
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-21 17:44:40 -0700

    add btn

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,33 +1,45 @@
 var userTurn = 'A';
+var winner = {'X':'A',
+              'O':'B'
+              }
 var currentBoard = [];
 var arr = document.getElementsByClassName("squre");
+var restBtn = document.getElementById("btn");
+restBtn.addEventListener("click", emptyB);
 //make empty board
 console.log(arr)
+function emptyB() {
+  for(var i = 0; i < arr.length;i++) {
+   arr[i].innerHTML = 0;
+}  
+}
 function myBoard() {
   var board = [];
   for(var i = 0; i < arr.length;i++) {
       var count = 0;
       board.push(arr[i].innerHTML.trim());
   }
-  currentBoard.push(board.slice(0,3));
-  currentBoard.push(board.slice(3,6));
-  currentBoard.push(board.slice(6,9));
+  currentBoard[0] = board.slice(0,3);
+  currentBoard[1] = board.slice(3,6);
+  currentBoard[2] = board.slice(6,9);
 }
   
 myBoard();
-console.log(currentBoard)
+
 
 function clickEvent(){
   if(userTurn === 'A') {
-    console.log(this)
-    console.log('innerText!!' + this.textContent)
    this.innerHTML = 'X';
+   myBoard();
   } else {
-  this.innerHTML = 'O';
+    this.innerHTML = 'O';
+    myBoard();
   }
- // myBoard();
+  myBoard();
   horizontally();
   vertically()
+  diagonally();
+  console.log(currentBoard)
 }
 
 function clickEventB(){
@@ -36,10 +48,7 @@ function clickEventB(){
 
 function switchUser(){
   userTurn === 'A' ? userTurn = 'B' : userTurn = 'A';
-  //console.log(userTurn);
 }
-
-//todo implement board
 
 function isItEmpty(){
   for(var i = 0; i < arr.length;i++) {
@@ -56,21 +65,29 @@ for(var i = 0; i < arr.length;i++) {
 isItEmpty();
 function horizontally(){
   if(currentBoard.some(e => e.join('') === 'XXX' || e.join('') === 'OOO')) {
-    alert('hiiiiiii')
+    alert('win')
   }
 }
 function vertically() {
-  for(var  i = 0; i < currentBoard.length;i++) {
+  for(var i = 0; i < currentBoard.length;i++) {
+    let col = [];
     for(var j = 0; j < currentBoard.length;j++) {
-      let arr = [];
-      arr.push([currentBoard[i][j]]);
+      col.push(currentBoard[j][i]);
     }
-   
+    if(col.join('').includes('XXX') || col.join('').includes('OOO')) {
+      alert('win')
+    }
   }
-  let first = currentBoard.filter((e) => e[i][0] === 0);
-
-  console.log('vertical ' + first)
+}
+function diagonally() {
+  var topToBottom = [currentBoard[0][0],currentBoard[1][1],currentBoard[2][2]];
+  var BottomToTOP = [currentBoard[0][2],currentBoard[1][1],currentBoard[2][0]];
+  console.log(topToBottom);
+  if(topToBottom.join('') === 'XXX' || BottomToTOP.join('') === 'XXX') {
+    alert('win')
+  }
+  if(topToBottom.join('') === 'OOO' || BottomToTOP.join('') === 'OOO') {
+    alert('win')
+  }
 }
 console.log('hi');
-console.log(currentBoard)
-//.addEventListener("click", clickEvent);
