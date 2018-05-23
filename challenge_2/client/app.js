01d5c5ff7ac9f8f06846b7cfb6ecb48e1697ef2a
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-22 17:09:28 -0700

    getting data from ajax

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,8 +1,28 @@
+//import { IncomingMessage } from "http";
+
+
 $(document).ready(function(){
 var handleSubmitBtn = $("button").on('click',function(e){
   e.preventDefault();
   var inputVal = $("#input").val();
-  console.log(intputVal)
+  var message = JSON.stringify({inputVal})
+
+  $.ajax({
+    url:"http://localhost:3000",
+    type: 'POST',
+    data: message,
+    contentType: "application/json",
+    success: function(data) {
+        console.log('success');
+        console.log(data)
+        //callback?
+        //append parsed data to results p
+        console.log('here is data', data);
+    },
+    err: function(err){
+      console.log(err);
+    }
+  });
 });
 });
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
-//app.get('/',(req,res) => res.send('HELLO WORLD'));
+const path    = require("path");
+var bodyParser = require('body-parser');
+
 app.use(express.static('client'));
-app.get('/',(req,res) => res.send('HELLO WORLD'));
+app.use(bodyParser.json());
+app.get('/',function(req,res) {
+console.log('-----------' + req)
+//console.log('--------------' + JSON.stringify(req.body));
+res.send(req.body)
+//res.send(req.data);
+});
+
 app.post('/', function (req, res) {
-  console.log(res);
-  res.send(res)
-})
+  ////console.log('--------------' + JSON.stringify(res));
+ // res.send('POST')
+ console.log(req.body)
+  res.send(req.body);
+});
 app.listen(3000,()=> console.log('hello'));
+
+
